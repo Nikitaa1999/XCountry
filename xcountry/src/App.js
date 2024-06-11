4e1aaf95511559e1f127e5c2aff334de72f5176f
unknown <nikita.phulkar@gmail.com>
2024-06-11 14:25:21 +0530

    Initial comit

---
+++ xcountry/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Country from './Country';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Country />
+    </div>
+  );
+}
+
+export default App;
---
+++ xcountry/src/Country.jsx
@@ -0,0 +1,123 @@
+import {useState, useEffect} from 'react'
+
+const Country=()=>{
+    const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedState, setSelectedState] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+
+  useEffect(() => {
+    try{
+    fetch('https://crio-location-selector.onrender.com/countries')
+      .then(response => response.json())
+      .then(data => { setCountries(data)
+        // setSelectedState('');
+        // setSelectedCity('');
+      });
+    }
+    catch(err){
+      console.log("error in 1st-",err);
+    }
+  }, []);
+ // console.log(countries);
+
+  useEffect(() => {
+    if (selectedCountry ) {
+      try{
+        console.log("selectedCountry===>",selectedCountry);
+      fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+        .then(response => response.json())
+        .then(data => {
+          setStates(data)
+          setSelectedState("");
+          setCities([]);  
+          setSelectedCity("");
+        });
+
+       
+      }
+      catch(err){
+        console.log("error  in 2nd-",err);
+      }
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedCountry && selectedState ) {
+
+      try{
+      fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
+        .then(response => response.json())
+        .then(data => {setCities(data)   
+          setSelectedCity("");
+          } );
+        }
+        catch(err ){
+          console.log("error in 3rd-",err)
+        }
+       
+    }
+  }, [selectedCountry, selectedState]);
+
+  // const handleCountryChange = e => {
+  //   console.log(e.target.value);
+  //   setSelectedCountry(e.target.value);
+  //   setSelectedState('');
+  // };
+
+  // const handleStateChange = e => {
+  //   setSelectedState(e.target.value);
+  // };
+    
+  return(
+  <div>
+  <div>
+    <h1>Select Location</h1>
+  </div>
+
+<select value={selectedCountry}  onChange={(e)=>setSelectedCountry(e.target.value)}>
+        <option value=""
+       >Select Country</option>
+        {countries.map(country => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+
+      <select value={selectedState} 
+        onChange={(e)=>setSelectedState(e.target.value)}
+        disabled={!selectedCountry}
+      >
+        <option value="" disabled>Select State</option>
+        {states.map(country => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+
+      <select 
+        value={selectedCity}
+        onChange={(e)=>setSelectedCity(e.target.value)}
+        disabled={!selectedState}>
+        <option value="" disabled>Select City</option>
+        {cities.map(city => (
+          <option key={city} value={city}>
+            {city}
+          </option>
+        ))}
+      </select>
+
+      {selectedCountry && selectedCity && selectedState ?
+       <div>
+          <p>You selected {selectedCountry}, {selectedState}, {selectedCity} </p>
+      </div>
+      :<></>}
+  </div>
+  );
+}
+
+export default Country;
